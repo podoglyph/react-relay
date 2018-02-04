47bfdfb41bd36390a6b04ded1ce201bb8336c7e1
Padraic O'Neill <pconeill@gmail.com [core]>
2018-02-04 15:33:11 -0700

    refators handleClick to accept any of the buttons

--- client/components/switch.js
+++ client/components/switch.js
@@ -4,16 +4,23 @@ import axios from 'axios';
 class Switch extends Component {
   constructor(props) {
     super(props);
-    this.state = { isToggleOn: true };
+    this.state = { isRedOn: true };
+    this.state = { isOrangeOn: true };
+    this.state = { isGreenOn: true };
+    this.state = { isBlueOn: true };
     this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick() {
+  handleClick(event) {
+    let button = event.target;
+    let ledIndex = button.getAttribute('data-led');
+    let stateSelector = button.getAttribute('data-color');
+
     this.setState(prevState => ({
-      isToggleOn: !prevState.isToggleOn
+      isRedOn: !prevState.isRedOn
     }));
 
-    axios.get('http://192.168.0.42:8080/leds/3').then(({ data })=> {
+    axios.get(`http://192.168.0.42:8080/leds/0${ledIndex}`).then(({ data })=> {
       	console.log(data);
       })
       .catch((err)=> {})
@@ -21,9 +28,32 @@ class Switch extends Component {
 
   render() {
     return (
-      <button onClick={this.handleClick}>
-        {this.state.isToggleOn ? 'ON' : 'OFF'}
-      </button>
+      <ul>
+        <li>
+          <button onClick={this.handleClick} className="led-button" data-led="0" data-color="Red">
+            Red:
+            {this.state.isRedOn ? 'ON' : 'OFF'}
+          </button>
+        </li>
+        <li>
+          <button onClick={this.handleClick} className="led-button" data-led="1" data-color="Orange">
+            Orange
+            {this.state.isOrangeOn ? 'ON' : 'OFF'}
+            </button>
+        </li>
+        <li>
+          <button onClick={this.handleClick} className="led-button" data-led="2" data-color="Green">
+            Green
+            {this.state.isGreenOn ? 'ON' : 'OFF'}
+            </button>
+        </li>
+        <li>
+          <button onClick={this.handleClick} className="led-button" data-led="3" data-color="Blue">
+            Blue
+            {this.state.isBlueOn ? 'ON' : 'OFF'}
+            </button>
+        </li>
+      </ul>
     )
   }
 }
