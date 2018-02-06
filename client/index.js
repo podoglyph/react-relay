933c3417dfc49abbdb4fcedebe8428c71eea742b
Padraic O'Neill <pconeill@gmail.com [core]>
2018-02-05 23:28:26 -0700

    adds video stream using img src

--- client/index.js
+++ client/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import Title from './components/title';
 import Switch from './components/switch';
 import Video from './components/video';
+import Video2 from './components/video2';
 
 class App extends Component {
 
