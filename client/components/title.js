c1abf18f9a83e509fe049f5ff68888606a82ff6e
Padraic O'Neill <pconeill@gmail.com [core]>
2018-02-07 15:33:22 -0700

    adjust Title component, reruns build

--- client/components/title.js
+++ client/components/title.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 
 class Title extends Component {
   render() {
-    return (
-     <div className="row header">
-      <h1>Tessel Onboard LEDs</h1>
-     </div>);
+    return
+      <div className="row header">
+        <h1>Tessel Onboard LEDs</h1>
+      </div>
   }
 }
 
